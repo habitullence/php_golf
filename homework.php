7c5bf2a693b43f5e816d90300888871242407fa2
Richard Jones <r13ckj@gmail.com>
2010-10-08 02:26:25 +0100

    Got it down to 254 by messing around with way bottles is set.

--- homework.php
+++ homework.php
@@ -2,20 +2,19 @@
 $a = 99;
 
 while ($a > 0) {
+	$e = $f = 'bottle';
 	if ($a > 1) {
 		$c = $a - 1;
 		$d = 'Take one down and pass it around';
+		$e .= 's';
 	}
 	else {
 		$c = 99;
 		$d = 'Go to the store and buy some more';
 	}
-	$e = $f = 'bottle';
-	$e .= ($a > 1) ? 's' : '';
-	$f .= ($c > 1) ? 's' : '';
+	if ($a != 2) $f .= 's'; 
 	$g = 'of beer';
 	$h = 'on the wall';
-	
 	echo "$a $e $g $h, $a $e $g.\n$d, $c $f $g $h.\n\n";
 	$a --;
 }
