0dd30711d24d8e4c4e818403e92a5ad026946d45
Richard Jones <r13ckj@gmail.com>
2010-10-08 01:51:36 +0100

    Reduced from 292 to 267 bytes by changing for loop to while.

--- homework.php
+++ homework.php
@@ -1,8 +1,9 @@
 <?php
-$a = array_reverse(range(1, 99));
-foreach ($a as $b) {
-	if ($b > 1) {
-		$c = $b - 1;
+$a = 99;
+
+while ($a > 0) {
+	if ($a > 1) {
+		$c = $a - 1;
 		$d = 'Take one down and pass it around';
 	}
 	else {
@@ -10,10 +11,11 @@ foreach ($a as $b) {
 		$d = 'Go to the store and buy some more';
 	}
 	$e = $f = 'bottle';
-	$e .= ($b > 1) ? 's' : '';
+	$e .= ($a > 1) ? 's' : '';
 	$f .= ($c > 1) ? 's' : '';
 	$g = 'of beer';
 	$h = 'on the wall';
 	
-	echo "$b $e $g $h, $b $e $g.\n$d, $c $f $g $h.\n\n";
+	echo "$a $e $g $h, $a $e $g.\n$d, $c $f $g $h.\n\n";
+	$a --;
 }
--- input.php
+++ input.php
@@ -1,1 +1,1 @@
-<?$a=array_reverse(range(1,99));foreach($a as $b){if($b>1){$c=$b-1;$d='Take one down and pass it around';}else{$c=99;$d='Go to the store and buy some more';}$e=$f='bottle';$e.=($b>1)?'s':'';$f.=($c>1)?'s':'';$g='of beer';	$h='on the wall';echo "$b $e $g $h, $b $e $g.\n$d, $c $f $g $h.\n\n";}
+<?$a=99;while($a>0){if($a>1){$c=$a-1;$d='Take one down and pass it around';}	else{$c=99;$d='Go to the store and buy some more';}$e=$f='bottle';$e.=($a>1)?'s':'';$f.=($c>1)?'s':'';$g='of beer';$h='on the wall';echo"$a $e $g $h, $a $e $g.\n$d, $c $f $g $h.\n\n";$a --;}
