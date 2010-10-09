ea64f4b4315275560bb588ba2ca81053e142ba0e
Richard Jones <r13ckj@gmail.com>
2010-10-09 04:06:08 +0100

    Captured result of first call to anonymous func to reuse inside double quotes.

--- 99_bottles/homework.php
+++ 99_bottles/homework.php
@@ -1,4 +1,4 @@
-<?$a=99;while($a){$b=function($a){return"$a bottl".($a>1?es:e)." of beer";};$c=" on the wall";echo$b($a).$c.", ".$b($a).".
+<?$a=99;while($a){$b=function($a){return"$a bottl".($a>1?es:e)." of beer";};$c=" on the wall";$d=$b($a);echo"$d$c, $d.
 ";echo$a>1?'Take one down and pass it around, '.$b($a-1):'Go to the store and buy some more, '.$b(99);echo"$c.
 
 ";$a--;}
