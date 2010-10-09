11f28800db3518f71b7323e45a1955a5af074625
Richard Jones <r13ckj@gmail.com>
2010-10-09 02:52:47 +0100

    Cleaned it up. Still 240.

--- 99_bottles/homework.php
+++ 99_bottles/homework.php
@@ -1,4 +1,4 @@
-<?$a=99;while($a){echo"$a bottl".($b=($a>1)?es:e).$c=" of beer";echo$d=" on the wall";echo", $a bottl$b$c.
-".(($a>1)?"Take one down and pass it around":"Go to the store and buy some more").", ";$a--;echo($b=($a>0)?$a:99)." bottl".(($b>1)?es:e)."$c$d.
+<?$a=99;$c=" of beer";$d=" on the wall";while($a){$b=$a>1?es:e;$e=$a>1?"Take one down and pass it around":"Go to the store and buy some more";echo"$a bottl$b$c$d, $a bottl$b$c.
+$e, ";$a--;echo($b=$a>0?$a:99)." bottl".($b>1?es:e)."$c$d.
 
 ";}
