133bd823274a0bf4daa79300dd671409d5217cc0
Richard Jones <r13ckj@gmail.com>
2010-10-09 04:32:27 +0100

    Removed extra newline. Now 235 bytes.

--- 99_bottles/homework.php
+++ 99_bottles/homework.php
@@ -1,5 +1,4 @@
 <?$a=99;while($a){$b=function($a){return"$a bottl".($a>1?es:e)." of beer";};$c=" on the wall";$d=$b($a);$e=$a>1?"Take one down and pass it around, {$b($a-1)}":"Go to the store and buy some more, {$b(99)}";echo"$d$c, $d.
 $e$c.
 
-";
-$a--;}
+";$a--;}
