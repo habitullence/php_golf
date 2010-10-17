6f8227052a57b66a6d8f39d677b8016804da464b
Richard Jones <r13ckj@gmail.com>
2010-10-17 15:53:29 +0100

    Change the way bottles is pluralised. Instead of tertiary conditional, I set the variable after the first bottle string is defined. 188 bytes and 2nd place :D

--- 99_bottles/homework.php
+++ 99_bottles/homework.php
@@ -1,12 +1,5 @@
-<?
-while($b<99){
-    $p=$b++?es:e;
-    $o="$b bottl$p of beer";
-    $w="$o on the wall";
-    $s="
+<?while($b<99){$b++;$o="$b bottle$p of beer";$p=s;$w="$o on the wall";$s="
 Take one down and pass it around, $w.
 
-$w, $o.$s";
-}
-echo substr("$s
-Go to the store and buy some more, $w.",66);
+$w, $o.$s";}echo substr("$s
+Go to the store and buy some more, $w.",68);
