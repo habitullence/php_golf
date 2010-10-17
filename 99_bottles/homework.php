6be860b19d4262bc118e764c910eddc3ce231f76
Richard Jones <r13ckj@gmail.com>
2010-10-17 14:59:14 +0100

    Instead of duplicate conditional for whether to add "Take one down..." line I now add it regardless and remove the first line on output. Down to 188 and 3rd place :)

--- 99_bottles/homework.php
+++ 99_bottles/homework.php
@@ -1,6 +1,12 @@
-<?while($b<99){$p=$b++?es:e;$o="$b bottl$p of beer";$w="$o on the wall";$s=($b<99?"
+<?
+while($b<99){
+    $p=$b++?es:e;
+    $o="$b bottl$p of beer";
+    $w="$o on the wall";
+    $s="
 Take one down and pass it around, $w.
 
-":"")."$w, $o.$s";
-}echo"$s
-Go to the store and buy some more, $w.";
+$w, $o.$s";
+}
+echo substr("$s
+Go to the store and buy some more, $w.",66);
