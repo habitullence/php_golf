5b82e0014f092b3913350133fe8f3d8d12483cf0
Richard Jones <r13ckj@gmail.com>
2010-10-19 23:20:01 +0100

    Started work on XML tree challenge.

--- xml_tree/homework.php
+++ xml_tree/homework.php
@@ -1,10 +1,5 @@
 <?
 error_reporting('E_ALL &~E_NOTICE');
-echo '
-';
 ob_start();
 include 'input.php';
 define('XML', ob_get_clean());
-preg_match_all('/<([a-z]*)\b/',XML,$m);
-foreach($m[1] as$n){echo"
-$p$n";$p.='    ';}
