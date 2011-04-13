96f7b61ba32e6b39306248d620c8e3fafcbcadcb
Richard Jones <r13ckj@gmail.com>
2011-04-13 13:42:10 +0100

    Added primes homework.

--- primes/homework.php
+++ primes/homework.php
@@ -1,2 +1,11 @@
-<?while($n++<541){preg_match('/^1?$|^(11+?)\1+$/',str_repeat(1,$n),$m);$m[0]||print"
-$n";}
+<?
+while($n++<541){
+    for($x=0,$t=2;$t<=541/2;$t++) {
+        if($n%$t==0)$x++;
+    }
+    if($x<2)echo"$x";
+}
+// Python nested for statement
+// 
+// r=range(2,99)
+// [x for x in r if sum(x%d<1 for d in r)<2]
